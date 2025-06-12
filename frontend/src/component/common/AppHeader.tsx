c328e28ad5af35df06d4848a9a1c3ff62d7b0c67
Rayd-kim <usk05@daum.net>
2025-06-12 21:56:14 +0900

    feat: header 로그인에 필요한 auth axios 적용 및 테스트

--- frontend/src/component/common/AppHeader.tsx
+++ frontend/src/component/common/AppHeader.tsx
@@ -1,27 +1,49 @@
+import logo_path from '../../assets/로고.png'
+import { loginAndGetJWT, logoutFromBE } from '../../api/axios-auth';
+import { useRef, useState } from 'react';
 type Props = {
 };
-import logo_path from '../../assets/로고.png'
 
-function LoginButton (isLoggedIn: boolean) {
-    if (!isLoggedIn) {
-        return (
-            <div>
-                <button>로그인</button>
-                <button>회원가입</button>
-            </div>
-        );
-    } else {
-        return (
-            <div>
-                <span>누구누구님, 안녕하세요</span>
-                <button>로그아웃</button>
-            </div>
-        );
-    }
-}
 
 const AppHeader: React.FC<Props> = () => {
-    const isLoggedIn:boolean = false;
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const refreshToken = useRef('');
+
+    async function Login () {
+        // const response = await loginAndGetJWT('ssafy', 'ssafy');
+        try {
+            const response = await loginAndGetJWT('ssafy', 'ssafy');
+            console.log(response.data);
+            setIsLoggedIn(!isLoggedIn);
+            refreshToken.current = response.data.tokens.refreshToken;
+        } catch (error) {
+            alert('id, password 확인하세요.')
+        }
+    }
+
+    async function Logout() {
+        const response = await logoutFromBE(refreshToken.current);
+        console.log(response.data)
+        setIsLoggedIn(!isLoggedIn);
+    }
+
+    function LoginButton() {
+        if (!isLoggedIn) {
+            return (
+                <div>
+                    <button onClick={Login}>로그인</button>
+                    <button>회원가입</button>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <span>누구누구님, 안녕하세요</span>
+                    <button onClick={Logout}>로그아웃</button>
+                </div>
+            );
+        }
+    }
 
     return (
         <div className="header">
@@ -35,7 +57,7 @@ const AppHeader: React.FC<Props> = () => {
             </div>
 
             <div className="auth">
-                {LoginButton(isLoggedIn)}
+                <LoginButton />
             </div>
 
         </div>
